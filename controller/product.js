79592a2e725baeeec74da30dc83b0e16c879a5e0
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2024-11-20 23:07:46 +0530

    delivered product

--- controller/product.js
+++ controller/product.js
@@ -81,18 +81,18 @@ exports.getAll = asyncHandler(async (req, res) => {
 });
 
 //get by Id
-// exports.get = asyncHandler(async (req, res) => {
-//   const product = await Product.findById(req.params.id);
-//   res.status(200).json(product);
-// });
 exports.get = asyncHandler(async (req, res) => {
-  const id = req.params.id;
-  const product = await Product.findById(id);
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
+  const product = await Product.findById(req.params.id);
   res.status(200).json(product);
 });
+// exports.get = asyncHandler(async (req, res) => {
+//   const id = req.params.id;
+//   const product = await Product.findById(id);
+//   if (!product) {
+//     return res.status(404).json({ message: "Product not found" });
+//   }
+//   res.status(200).json(product);
+// });
 
 
 //update product
